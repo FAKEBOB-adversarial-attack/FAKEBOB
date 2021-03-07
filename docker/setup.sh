c69ff0295ac1e21973f17653231f4ddccec06218
Tom Dörr <tom.doerr@tum.de>
2021-03-07 11:46:39 +0100

    Update file

--- docker/setup.sh
+++ docker/setup.sh
@@ -77,11 +77,9 @@ fi
 
 docker build docker -t fakebob
 pip3 install gdown
-gdown https://drive.google.com/u/0/uc?id=1nEcobGN7_8yyYwdqs1c6XD1UTXqEyhQC&export=download
-tar xzf pre-models.tgz
+gdown https://drive.google.com/u/0/uc?id=1nEcobGN7_8yyYwdqs1c6XD1UTXqEyhQC&export=download && tar xzf pre-models.tgz
 
-gdown https://drive.google.com/u/0/uc?id=1T_hx9Pqopk-rlmiSrBWdXjl825wjBQVF&export=download
-tar xzf data.tar.gz
+gdown https://drive.google.com/u/0/uc?id=1T_hx9Pqopk-rlmiSrBWdXjl825wjBQVF&export=download && tar xzf data.tar.gz
 
 docker/run.sh python3 build_spk_models.py
 
