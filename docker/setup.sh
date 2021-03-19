1c33545be38eaab41cb42bbb6d67affe0451fa0a
Tom Dörr <tom.doerr@tum.de>
2021-03-19 08:19:09 +0100

    Add check whether docker is installed

--- docker/setup.sh
+++ docker/setup.sh
@@ -5,6 +5,12 @@ then
     cd ..
 fi
 
+if ! command -v docker &> /dev/null
+then
+    echo "Docker doesn't seem to be installed on your system. The docker/setup.sh script uses docker to set up the environment, but can't install docker itself. Please install docker."
+    exit
+fi
+
 docker build docker -t fakebob
 pip3 install gdown
 gdown https://drive.google.com/u/0/uc?id=1T_hx9Pqopk-rlmiSrBWdXjl825wjBQVF&export=download
